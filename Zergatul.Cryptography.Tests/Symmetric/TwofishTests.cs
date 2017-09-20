ce6ae2094d20063d6c799ab6d5d6b9e532145f4d
Igor Budzhak <z3rgatul@gmail.com>
2017-09-21 01:52:38 +0300

    Twofish

---
+++ Zergatul.Cryptography.Tests/Symmetric/TwofishTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zergatul.Cryptography.Symmetric;
+
+namespace Zergatul.Cryptography.Tests.Symmetric
+{
+    [TestClass]
+    public class TwofishTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestEncryptDecrypt(
+                "00000000000000000000000000000000",
+                "00000000000000000000000000000000",
+                "00000000000000000000000000000000");
+        }
+
+        private static void TestEncryptDecrypt(string key, string plaintext, string ciphertext)
+        {
+            byte[] bkey = BitHelper.HexToBytes(key.Replace(" ", ""));
+            byte[] bplain = BitHelper.HexToBytes(plaintext.Replace(" ", ""));
+            byte[] bcipher = BitHelper.HexToBytes(ciphertext.Replace(" ", ""));
+
+            var tf = new Twofish(bkey.Length);
+
+            var enc = tf.CreateEncryptor(bkey);
+            Assert.IsTrue(bcipher.SequenceEqual(enc(bplain)));
+
+            var dec = tf.CreateDecryptor(bkey);
+            Assert.IsTrue(bplain.SequenceEqual(dec(bcipher)));
+        }
+    }
+}
--- Zergatul/Cryptography/Symmetric/Twofish.cs
+++ Zergatul/Cryptography/Symmetric/Twofish.cs
@@ -13,6 +13,14 @@ namespace Zergatul.Cryptography.Symmetric
         public override int BlockSize => 16;
         public override int KeySize => _keySize;
 
+        private static readonly byte[,] RS = new byte[,]
+        {
+            { 0x01, 0xA4, 0x55, 0x87, 0x5A, 0x58, 0xDB, 0x9E },
+            { 0xA4, 0x56, 0x82, 0xF3, 0x1E, 0xC6, 0x68, 0xE5 },
+            { 0x02, 0xA1, 0xFC, 0xC1, 0x47, 0xAE, 0x3D, 0x19 },
+            { 0xA4, 0x55, 0x87, 0x5A, 0x58, 0xDB, 0x9E, 0x03 },
+        };
+
         private static readonly byte[,] MDS = new byte[,]
         {
             { 0x01, 0xEF, 0x5B, 0x5B },
@@ -25,7 +33,52 @@ namespace Zergatul.Cryptography.Symmetric
 
         public Twofish(int keySize)
         {
+            switch (keySize)
+            {
+                case 16:
+                case 24:
+                case 32:
+                    break;
+                default:
+                    throw new InvalidOperationException("Only 16/24/32 byte length keys are supported in Twofish");
+            }
+
             this._keySize = keySize;
+        }
+
+        private static void KeySchedule(byte[] key)
+        {
+            int k = key.Length / 8;
+            uint[] M = new uint[2 * k];
+            for (int i = 0; i < M.Length; i++)
+                BitHelper.ToUInt32(key, i * 4, ByteOrder.LittleEndian);
+
+            var bf = BinaryPolynomial.FromPowers(8, 6, 3, 2, 0);
+
+            byte[] s = new byte[key.Length / 2];
+            for (int si = 0; si < k; si++)
+            {
+                var z = new BinaryPolynomial[]
+                {
+                    BinaryPolynomial.Zero,
+                    BinaryPolynomial.Zero,
+                    BinaryPolynomial.Zero,
+                    BinaryPolynomial.Zero
+                };
+                for (int i = 0; i < 4; i++)
+                    for (int j = 0; j < 8; j++)
+                        z[i] += BinaryPolynomial.ModularMultiplication(
+                            new BinaryPolynomial(new byte[] { key[8 * si + j] }, ByteOrder.LittleEndian),
+                            new BinaryPolynomial(new byte[] { MDS[i, j] }, ByteOrder.LittleEndian),
+                            bf);
+                s[si * 4 + 0] = z[0].ToBytes(ByteOrder.LittleEndian, 1)[0];
+                s[si * 4 + 1] = z[1].ToBytes(ByteOrder.LittleEndian, 1)[0];
+                s[si * 4 + 2] = z[2].ToBytes(ByteOrder.LittleEndian, 1)[0];
+                s[si * 4 + 3] = z[3].ToBytes(ByteOrder.LittleEndian, 1)[0];
+            }
+            uint[] S = new uint[k];
+            for (int i = 0; i < k; i++)
+                BitHelper.ToUInt32(s, (k - 1 - i) * 4, ByteOrder.LittleEndian);
         }
 
         delegate void FFunction(uint r0, uint r1, int r, out uint f0, out uint f1);
@@ -98,6 +151,11 @@ namespace Zergatul.Cryptography.Symmetric
                     F(x0, x1, round, out f0, out f1);
                     uint y0 = BitHelper.RotateRight(x1 ^ f0, 1);
                     uint y1 = BitHelper.RotateLeft(x3, 1) ^ f1;
+
+                    x0 = y0;
+                    x1 = y1;
+                    x2 = x0;
+                    x3 = x1;
                 }
 
                 x0 ^= k[4];
