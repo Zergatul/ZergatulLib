de5e9f2d2e291f8e26bedc90631fc1c0b8bd2a8a
Igor Budzhak <z3rgatul@gmail.com>
2018-03-12 00:37:22 +0200

    AttributePair for certificates

---
+++ Zergatul/Cryptography/Certificate/AttributePair.cs
@@ -0,0 +1,14 @@
+﻿namespace Zergatul.Cryptography.Certificate
+{
+    public class AttributePair<TKey, TValue>
+    {
+        public TKey Key { get; private set; }
+        public TValue Value { get; private set; }
+
+        public AttributePair(TKey key, TValue value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
---
+++ Zergatul/Cryptography/Certificate/GeneralSubtree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zergatul.Math;
+using Zergatul.Network.Asn1;
+
+namespace Zergatul.Cryptography.Certificate
+{
+    public class GeneralSubtree
+    {
+        public GeneralName Base { get; private set; }
+        public BigInteger Mininum { get; private set; }
+        public BigInteger Maximum { get; private set; }
+
+        internal GeneralSubtree(Asn1Element element)
+        {
+            var seq = element as Sequence;
+            ParseException.ThrowIfNull(seq);
+            ParseException.ThrowIfNotInRange(seq.Elements.Count, 1, 3);
+
+            Base = new GeneralName(seq.Elements[0]);
+
+            if (seq.Elements.Count >= 2)
+            {
+                var integer = seq.Elements[1] as Integer;
+                ParseException.ThrowIfNull(integer);
+                Mininum = integer.Value;
+            }
+            else
+                Mininum = BigInteger.Zero;
+
+            if (seq.Elements.Count >= 3)
+            {
+                var integer = seq.Elements[2] as Integer;
+                ParseException.ThrowIfNull(integer);
+                Maximum = integer.Value;
+            }
+        }
+    }
+}
